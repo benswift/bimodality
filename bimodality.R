2a8bebb69a6faa41eb8f6d7e8831d308516b0dd4
Ben Swift <ben@benswift.me>
2020-07-01 15:39:05 +1000

    add a bunch more ANU-specific analysis code
    and a bit of general code cleanup as well - I should probably get back to this
    later and merge the relevant bits into master. oh well.

--- bimodality.R
+++ bimodality.R
@@ -9,7 +9,8 @@ library("moments")
 ## load data ##
 ###############
 
-source("fake_data.R")
+## source("fake_data.R")
+source("preprocessing.R")
 
 #####################
 ## calculate stats ##
@@ -19,8 +20,8 @@ stats = data %>%
   filter(!is.na(mark)) %>%
   group_by(institution, year, term, course) %>%
   ## ad.test sample size must be greater than 7
-  filter(length(mark) > 7) %>%
-  summarize(n = length(mark),
+  filter(n() > 7) %>%
+  summarize(n = n(),
             mean = mean(mark),
             sd = sd(mark),
             kurtosis = kurtosis(mark),
@@ -45,38 +46,85 @@ stats = data %>%
 stats %>%
   group_by(institution) %>%
   summarise(percent_shapiro_rejected = mean(p_shapiro < 0.05) * 100,
-            percent_dip_test_rejected = mean(kurtosis < 3 & p_dip < 0.05) * 100)
+            percent_dip_test_rejected = mean(kurtosis < 3 & p_dip < 0.05) * 100) %>%
+  print(n=Inf)
 
 ###################
 ## visualisation ##
 ###################
 
-## theme_set(theme_gray(base_size = 25))
+theme_set(theme_gray(base_size = 25))
 
-ggplot(data, aes(mark)) +
-  geom_histogram(breaks = seq(0, 100, 5)) +
+mark_histogram = geom_histogram(aes(fill = grade), breaks = seq(0, 100, 1), closed="left")
+
+data %>%
+  ggplot(aes(mark)) +
+  mark_histogram +
+  scale_fill_brewer(type="qual", palette = 2) +
   labs(title = "all marks")
 
-ggplot(data, aes(mark)) +
-  geom_histogram(breaks = seq(0, 100, 5)) +
-  facet_wrap(~institution) +
-  labs(title = "marks by institution")
-
-ggplot(data, aes(mark)) +
-  geom_histogram(breaks = seq(0, 100, 5)) +
+data %>%
+  ggplot(aes(mark)) +
+  mark_histogram +
+  scale_fill_brewer(type="qual", palette = 2) +
   facet_wrap(~year) +
-  labs(title = "marks by year")
+  labs(title = "marks by year") +
+  scale_x_continuous(n.breaks = 2) +
+  guides(x = guide_axis(angle = 90))
 
-ggplot(data, aes(mark)) +
-  geom_histogram(breaks = seq(0, 100, 5)) +
-  facet_grid(institution~year) +
-  labs(title = "marks by year & institution")
+data %>%
+  filter(residency %in% c("AUS", "INTL")) %>%
+  ggplot(aes(mark)) +
+  mark_histogram +
+  scale_fill_brewer(type="qual", palette = 2) +
+  facet_grid(residency~year) +
+  labs(title = "marks by year & residency") +
+  scale_x_continuous(n.breaks = 2) +
+  guides(x = guide_axis(angle = 90))
+
+data %>%
+  filter(gender %in% c("F", "M")) %>%
+  ggplot(aes(mark)) +
+  mark_histogram +
+  scale_fill_brewer(type="qual", palette = 2) +
+  facet_wrap(~gender)
+
+data %>%
+  filter(gender %in% c("F", "M")) %>%
+  ggplot(aes(mark)) +
+  mark_histogram +
+  facet_grid(gender~year) +
+  scale_x_continuous(n.breaks = 2) +
+  guides(x = guide_axis(angle = 90))
 
 ## this won't really be useful for the fake data (since the distributions don't
 ## change year-to-year) but it might be interesting for real data
 
-ggplot(data, aes(year, mark)) +
+data %>%
+  ggplot(aes(year, mark)) +
   geom_point(alpha = 0.01) +
   geom_smooth(method = "lm") +
-  facet_wrap(~institution) +
   labs(title = "marks over time")
+
+## transform(year = cut(year, breaks = seq(1995, 2020, 5), right = FALSE)) %>%
+
+data %>%
+  group_by(program) %>%
+  count() %>%
+  filter(n > 100) %>%
+  arrange(n) %>%
+  print(n=Inf)
+
+data %>%
+  filter(gender %in% c("F", "M")) %>%
+  group_by(year, gender) %>%
+  count() %>%
+  ggplot(aes(year, n, colour = gender)) +
+  geom_line(size = 3) +
+  scale_colour_brewer(type="qual")
+
+data %>%
+  filter(gender %in% c("F", "M")) %>%
+  ggplot(aes(gender, fill = gender)) +
+  geom_bar() +
+  facet_wrap(~year)
